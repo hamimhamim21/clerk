5d1e22b38588a570e964e1dba1465b55d01d075a
Matthew Segal <mattdsegal@gmail.com>
2020-05-08 14:15:44 +1000

    add twilio POC code

--- app/clerk/urls.py
+++ app/clerk/urls.py
@@ -4,13 +4,21 @@ from django.urls import path
 from rest_framework import routers
 
 from questions.views import apis
-from webhooks.views import webflow_form_view
+from webhooks.views import (
+    webflow_form_view,
+    twilio_call_view,
+    twilio_end_call_view,
+    twilio_record_call_view,
+)
 
 router = routers.SimpleRouter()
 router.register("submission", apis.SubmissionViewSet, basename="submission")
 router.register("images", apis.ImageUploadViewSet, basename="images")
 urlpatterns = [
     path("admin/", admin.site.urls, name="admin"),
+    path("twilio/", twilio_call_view),
+    path("twilio/end/", twilio_end_call_view),
+    path("twilio/record/", twilio_record_call_view),
     path("api/webhooks/webflow-form/", webflow_form_view, name="webflow-form"),
     path("api/", include(router.urls)),
 ]
--- app/webhooks/views.py
+++ app/webhooks/views.py
@@ -1,12 +1,87 @@
 import logging
 
+from django.views.decorators.http import require_http_methods
 from rest_framework.exceptions import ValidationError
 from rest_framework.response import Response
 from rest_framework.decorators import api_view
 
+
+from django.http import HttpResponseBadRequest, HttpResponse
+
 from .models import WebflowContact
 
 logger = logging.getLogger(__file__)
+
+from twilio.rest import Client
+
+from twilio.twiml.voice_response import VoiceResponse, Gather
+
+
+@require_http_methods(["GET"])
+def twilio_call_view(request):
+    r = VoiceResponse()
+    dial_number_str = request.GET.get("Digits")
+    if dial_number_str:
+        r.say(f"Connecting you to dialled number.")
+        if dial_number_str.startswith("04"):
+            dial_number_str = "+61" + dial_number_str[1:]
+        r.dial(
+            dial_number_str,
+            method="GET",
+            action="/twilio/end/",
+            record="record-from-answer",
+            recordingStatusCallback="/twilio/record/",
+        )
+    else:
+        gather = Gather(action="/twilio/", method="GET", timeout=10)
+        gather.say("Dial a number then press hash.")
+        r.append(gather)
+
+    return TwimlResponse(r)
+
+
+from django.views.decorators.csrf import csrf_exempt
+
+
+@require_http_methods(["GET", "POST"])
+@csrf_exempt
+def twilio_record_call_view(request):
+    """
+    Receive recalling status callbacl
+    request.POST
+    <QueryDict: {
+        'RecordingSource': ['DialVerb'],
+        'RecordingSid': ['zzzz'],
+        'RecordingUrl': ['zz'],
+        'RecordingStatus': ['completed'],
+        'RecordingChannels': ['1'],
+        'ErrorCode': ['0'],
+        'CallSid': ['zzzz'],
+        'RecordingStartTime': ['Sat, 02 May 2020 00:45:44 +0000'],
+        'AccountSid': ['zzz'],
+        'RecordingDuration': ['7']
+    }>
+    """
+    return HttpResponse("Success?")
+
+
+@require_http_methods(["GET"])
+def twilio_end_call_view(request):
+    """Thank user & hang up."""
+    r = VoiceResponse()
+    r.say("Successful call.")
+    r.hangup()
+    return TwimlResponse(r)
+
+
+class TwimlResponse(HttpResponse):
+    """
+    HTTP response returning Twilio Markup Language (TwiML)
+    """
+
+    def __init__(self, twiml_obj, **kwargs):
+        super_kwargs = {"status": 200, "content_type": "application/xml", **kwargs}
+        return super().__init__(str(twiml_obj), **super_kwargs)
 
 
 @api_view(["POST"])
